00d2cd48ef07b0b235693162106fcb6f323acdb7
virat <viratt737@gmail.com>
2025-11-20 02:53:53 +0530

    write our bookschema

--- Database/db.js
+++ Database/db.js
@@ -6,7 +6,7 @@ const connectToDB = async () => {
     console.log("MongoDB Connected Successfully");
   } catch (err) {
     console.error("MongoDB Connection Failed", err);
-    process.exit(1);  // stops server if DB fails
+    process.exit(1);  
   }
 };
 
--- Models/book.js
+++ Models/book.js
@@ -1,4 +1,24 @@
 const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
-    
-})
+    title:{
+        type:String,
+        required :['true','Book title is required'],
+        trim: true,
+        maxlength:[100,'Book title cannot be more than 100 characters' ],
+    },
+    author:{
+        type:String,
+        required :['true','author name is required'],
+        trim: true,
+    },
+    year:{
+        type:Number,
+        required:['true','year should be required '],
+    },
+    createdAt:{
+        type:Date,
+        default: Date.now,
+    }
+});
+
+module.exports = mongoose.model('Book',bookSchema);
