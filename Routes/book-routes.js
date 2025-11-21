3193559346161c0c87785bcfea136d1a12d922a0
virat <viratt737@gmail.com>
2025-11-21 21:01:10 +0530

    add and write file for controller and routes

---
+++ Routes/book-routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const {getAllBooks, getSingleBooksById,addNewBooks, updateBooks,deleteBooks} = require('../controllers/book-controller')
+const router = express.Router(); 
+
+// all the routes that required 
+router.get('/get',getAllBooks);
+router.get('/get/:id',getSingleBooksById);
+router.post('/add',addNewBooks);
+router.put('/update/:id',updateBooks);
+router.delete('/delete/:id',deleteBooks);
---
+++ controllers/book-controller.js
@@ -0,0 +1,21 @@
+const getAllBooks = async (req, res)=>{
+
+}
+
+const getSingleBooksById = async (req, res)=>{
+
+}
+
+const addNewBooks = async (req, res)=>{
+
+}
+
+const updateBooks = async (req, res)=>{
+
+}
+
+const deleteBooks = async (req, res)=>{
+
+}
+
+module.exports ={ getAllBooks, getSingleBooksById, addNewBooks, updateBooks, deleteBooks};
